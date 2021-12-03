bfdd5c248f48740c15919d581cb8760c002b9eae
diy <tsctan@foxmail.com>
2021-12-03 18:23:06 +0800

    .

--- readme.md
+++ readme.md
@@ -1,3 +1,7 @@
+## 介绍
+可以自动检测 torch 和 tensorflow 版本, 并自动设置环境变量相应的 cuda, 同时自动选择占用显存最低的显卡运行.
+在 anaconda 环境下使用.
+
 ## 安装
 pip install tsc-auto
 
@@ -6,24 +10,19 @@ pip install tsc-auto
 - 自动选择最低显存占用显卡跑 torch 的 cnn.py 程序: ta -p -a cnn.py
 - 手动选择编号0和1的显卡跑 tensorflow 的 cnn.py 程序: ta -t -d 0,1 -a cnn.py
 
-## cuda的位置必须在主目录 (可以使用 ln -s 软链接)
+## cuda的位置必须在用户主目录 (可以使用 ln -s 软链接)
 - ...
 - ~/cuda/8.0
 - ...
 - ~/cuda/11.3
 - ...
 
-## cuda 和 cudnn 拷贝的例子
+## 下载处理好的cuda
+- 链接: https://pan.baidu.com/s/1tXzED_8GZjJzm_SFzMTbKA 提取码: 3edj
+
+## 如果自行安装 cuda 和 cudnn, 需要处理的方式
 - cd ~/cuda
 - mv /usr/local/cuda-11.0/ 11.0
 - cp cudnn-v8.2.0.53/include/cudnn.h 11.0/include/
 - cp cudnn-v8.2.0.53/lib64/libcudnn* 11.0/lib64
 - chmod a+r 11.0/include/cudnn.h 11.0/lib64/libcudnn*
-
-## 下载处理好的cuda
-- 链接: https://pan.baidu.com/s/1tXzED_8GZjJzm_SFzMTbKA 提取码: 3edj
-
-### 暂时无用的用于超算的文件
-- info.py
-- query.py
-- srun.sh
--- setup.py
+++ setup.py
@@ -1,20 +1,15 @@
 # -*- coding: utf-8 -*-
 from setuptools import setup, find_packages
 
-LONGDOC = """
-Auto selector for NVIDIA GPUs and CUDA
-
-Support detection for tensorflow or torch
-"""
-
 setup(
     name='tsc-auto',
-    version='0.6',
-    description="tanshicheng's tools",
-    long_description=LONGDOC,
+    version='0.6.2',
+    description="Auto selector for GPU and CUDA, support the detection of tensorflow or torch",
+    long_description=open('readme.md', 'r', encoding='utf8').read(),
+    long_description_content_type="text/markdown",
     author='tanshicheng',
     license='GPLv3',
-    url='https://github.com/aitsc',
+    url='https://github.com/aitsc/tsc-auto',
     keywords='tools',
     packages=find_packages(),
     include_package_data=True,  # 包含 .sh 文件
@@ -25,7 +20,7 @@ setup(
         'Topic :: Software Development :: Libraries',
     ],
     entry_points={  # 打包到bin
-        'console_scripts': ['ta=tsc_auto.auto:main'],  # 包不能有-
+        'console_scripts': ['ta=tsc_auto.auto:main'],  # 包不能有-符号
     },
     python_requires='>=3.6',
 )
