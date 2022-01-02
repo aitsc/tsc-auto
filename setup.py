754d9ab37646528152ee8be6bea61da0af071a4e
diy <tsctan@foxmail.com>
2022-01-02 15:52:15 +0800

    修改默认参数限制

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ else:
 
 setup(
     name='tsc-auto',
-    version='0.8.3',
+    version='0.9',
     description="Auto selector for GPU and CUDA, support the detection of tensorflow or torch",
     long_description=long_description,
     long_description_content_type="text/markdown",
