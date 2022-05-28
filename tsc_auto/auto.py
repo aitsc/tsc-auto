c3f2e5158ad75bfa8fdb7d66d70a0b95d58a22fe
diy <tsctan@foxmail.com>
2022-05-28 15:33:09 +0800

    ta转义符号优化

--- tsc_auto/auto.py
+++ tsc_auto/auto.py
@@ -5,6 +5,14 @@ import subprocess
 
 def main():
     para = sys.argv[1:]
+    # 一些转义符号复原
+    for i, p in enumerate(para):
+        if '\\' in p:
+            para[i] = para[i].replace('\\', '\\\\')
+        if '"' in p:
+            para[i] = '"' + para[i].replace('"', '\\"') + '"'
+        if '$' in p:
+            para[i] = para[i].replace('$', '\\\\\\$')  # 因为 shell eval 所以多加入转义
     # 自动寻找 auto.sh 的位置
     auto = subprocess.getstatusoutput('py=$(which python) && echo ${py%bin*}lib/python*/site-packages/tsc_auto/auto.sh')
     # 没有报错
@@ -13,7 +21,8 @@ def main():
     else:
         raise NameError(str(auto) + ' 寻找auto.sh错误!')
     # 运行
-    os.system(f'chmod 777 {auto} && {auto} ' + ' '.join(para))
+    cmd = f'chmod 777 {auto} && {auto} ' + ' '.join(para)
+    os.system(cmd)
 
 
 if __name__ == '__main__':
